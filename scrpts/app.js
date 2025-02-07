e59f6598762980528970d3f0a713985ae189247e
= <=>
2025-02-08 00:47:25 +0300

    updating UI

--- scrpts/app.js
+++ scrpts/app.js
@@ -5,6 +5,22 @@ const card = document.querySelector(".card");
 const year = document.querySelector("footer");
 
 
+//updating UI
+const updateUI = (weatherData) => {
+    details.innerHTML = `
+        <h5 class="my-3"><span>${
+          weatherData.name
+        }</span>, <span>${convertCountryCode(weatherData.sys.country)}</span></h5>
+        <p class="my-3">${convertTimeStamp(
+          weatherData.dt,
+          weatherData.timezone
+        )}</p>
+        <div class="my-3">${weatherData.weather[0].main}</div>
+        <div class="display-4 my-4">
+          <span>${weatherData.main.temp}</span><span>&deg;C</span>
+        </div>
+    `;
+  };
 // Add event listener to the form
 
 cityForm.addEventListener("submit", async (e) => {
