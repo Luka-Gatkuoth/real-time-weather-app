3509a8bffff5ea1fc5f890db7f3dfa56629f6493
= <=>
2025-02-08 01:16:48 +0300

    querry image with class of time and class of icon

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ const cityForm = document.querySelector("form");
 const details = document.querySelector(".details");
 const card = document.querySelector(".card");
 const year = document.querySelector("footer");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img")
 
 // convert country code to name
 const convertCountryCode = (country) => {
