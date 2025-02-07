e5147448543bd7b33bb7e1d1c04c5f7c10c9f444
= <=>
2025-02-08 00:42:57 +0300

    querry dom

---
+++ scrpts/app.js
@@ -0,0 +1,5 @@
+// query document
+const cityForm = document.querySelector("form");
+const details = document.querySelector(".details");
+const card = document.querySelector(".card");
+const year = document.querySelector("footer");
